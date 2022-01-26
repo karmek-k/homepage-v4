4ca6439cee638476ca3f262eb25ca34e2e515a21
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-01-26 19:16:18 +0100

    Create Container component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import type { NextPage } from 'next';
 import { AsciiHeader } from '../components/AsciiHeader';
+import { Container } from '../components/Container';
 
 const Home: NextPage = () => {
   return (
     <>
-      <AsciiHeader />
+      <Container>
+        <header>
+          <AsciiHeader />
+        </header>
+        <main>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ducimus,
+          quis fugiat laudantium amet tempore adipisci perferendis veritatis
+          repudiandae, totam omnis commodi similique veniam at? Officia animi
+          sequi dolores nemo? Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Deleniti cupiditate totam vero eum, unde, dolor dolorum iusto,
+          facilis quo minus blanditiis quae quidem qui sed facere? Excepturi
+          laborum enim dolor!
+        </main>
+      </Container>
     </>
   );
 };
