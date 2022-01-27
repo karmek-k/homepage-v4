0004417cf3559feff15409af8eb26b21887ebd0e
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-01-27 14:30:12 +0100

    Extract the paragraph on hackers to a component

---
+++ components/HackerText.tsx
@@ -0,0 +1,28 @@
+export function HackerText() {
+  return (
+    <div className="">
+      <i>
+        <p>
+          A hacker is someone who enjoys playful cleverness—not necessarily with
+          computers. The programmers in the old MIT free software community of
+          the 60s and 70s referred to themselves as hackers. Around 1980,
+          journalists who discovered the hacker community mistakenly took the
+          term to mean “security breaker.”
+        </p>
+
+        <p>
+          Please don&apos;t spread this mistake. People who break security are
+          “crackers.”
+        </p>
+      </i>
+
+      <p>
+        (from{' '}
+        <a href="https://www.gnu.org/philosophy/words-to-avoid.en.html#Hacker">
+          gnu.org
+        </a>
+        )
+      </p>
+    </div>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { AsciiHeader } from '../components/AsciiHeader';
 import { Container } from '../components/Container';
 import { FooterLink } from '../components/FooterLink';
 import Head from 'next/head';
+import { HackerText } from '../components/HackerText';
 
 const Home: NextPage = () => {
   return (
@@ -17,28 +18,7 @@ const Home: NextPage = () => {
           <AsciiHeader />
         </header>
         <main>
-          <i>
-            <p>
-              A hacker is someone who enjoys playful cleverness—not necessarily
-              with computers. The programmers in the old MIT free software
-              community of the 60s and 70s referred to themselves as hackers.
-              Around 1980, journalists who discovered the hacker community
-              mistakenly took the term to mean “security breaker.”
-            </p>
-
-            <p>
-              Please don&apos;t spread this mistake. People who break security
-              are “crackers.”
-            </p>
-          </i>
-
-          <p>
-            (from{' '}
-            <a href="https://www.gnu.org/philosophy/words-to-avoid.en.html#Hacker">
-              gnu.org
-            </a>
-            )
-          </p>
+          <HackerText />
         </main>
         <footer>
           <FooterLink href="//github.com/karmek-k/homepage-v4" />
