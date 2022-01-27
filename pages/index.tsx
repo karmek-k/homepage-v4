2f8db955f8c776397dd93726d05427936380ff57
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2022-01-27 14:25:01 +0100

    Add a paragraph about hackers

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,13 +17,28 @@ const Home: NextPage = () => {
           <AsciiHeader />
         </header>
         <main>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ducimus,
-          quis fugiat laudantium amet tempore adipisci perferendis veritatis
-          repudiandae, totam omnis commodi similique veniam at? Officia animi
-          sequi dolores nemo? Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Deleniti cupiditate totam vero eum, unde, dolor dolorum iusto,
-          facilis quo minus blanditiis quae quidem qui sed facere? Excepturi
-          laborum enim dolor!
+          <i>
+            <p>
+              A hacker is someone who enjoys playful cleverness—not necessarily
+              with computers. The programmers in the old MIT free software
+              community of the 60s and 70s referred to themselves as hackers.
+              Around 1980, journalists who discovered the hacker community
+              mistakenly took the term to mean “security breaker.”
+            </p>
+
+            <p>
+              Please don&apos;t spread this mistake. People who break security
+              are “crackers.”
+            </p>
+          </i>
+
+          <p>
+            (from{' '}
+            <a href="https://www.gnu.org/philosophy/words-to-avoid.en.html#Hacker">
+              gnu.org
+            </a>
+            )
+          </p>
         </main>
         <footer>
           <FooterLink href="//github.com/karmek-k/homepage-v4" />
